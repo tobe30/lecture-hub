37cfd1d2495f212e1d4472a8778ebd5910243bb0
Tobe Marizu <tobemarizu@gmail.com>
2026-04-07 02:31:15 +0100

    first commit 2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
     withCredentials: true, // Include cookies in requests
 })
